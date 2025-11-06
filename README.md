88ced623d764d5665e702a5925a9d28fe7ea3e0f
Thenemex <tnmx777@gmail.com>
2025-11-06 23:56:10 +0100

    Initial commit with basic files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# Thaumcraft-NEI-Plugin-Patched
-An addon with bug-patches for the popular 1.7.10 mod Thaumcraft NEI Plugin, made by Jan2048.
+# Thaumcraft NEI Plugin : Patched
+## Addon for Thaumcraft NEI Plugin, made by Thenemex
+### Patched bugs :
+- Infusion, Crucible and both Shaped & Shapeless Arcane used to have issues not showing all recipes in the original mod. This was caused by the mod comparing the stackSize from the item clicked and all recipes.
+    - For exemple, Crucible recipe for Thauminite [Thaumic Bases] yields 8 nuggets. If you clicked the Thauminite nugget in NotEnoughItems menu, it would show you no recipe, as the item you click in NEI only got a stackSize of 1, and the output of that recipe got a stackSize of 8.
---
+++ src/main/java/thaumcraftneipluginpatched/ThaumcraftNEIPluginPatched.java
@@ -0,0 +1,27 @@
+package thaumcraftneipluginpatched;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPostInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+import static thaumcraftneipluginpatched.ThaumcraftNEIPluginPatched.dependencies;
+import static thaumcraftneipluginpatched.ThaumcraftNEIPluginPatched.modID;
+
+@SuppressWarnings({"unused", "EmptyMethod"})
+@Mod(modid = modID, useMetadata = true, version = "1.0", dependencies = dependencies)
+public class ThaumcraftNEIPluginPatched {
+
+    public static final String modID = "thaumcraftneipluginpatched", modName = "ThaumcraftNEIPluginPatched";
+
+    @Mod.EventHandler
+    public void preInit(FMLPreInitializationEvent ignoredEvent) {}
+
+    @Mod.EventHandler
+    public void init(FMLInitializationEvent ignoredEvent) {}
+
+    @Mod.EventHandler
+    public void postInit(FMLPostInitializationEvent ignoredEvent) {}
+
+    public static final String dependencies = "required-after:thaumcraftneiplugin@[1.7.10-1.7a,);required-after:NotEnoughItems@[1.0.5.120,)";
+}
