6a324b79897afe76535531758ea2bd9e5a3113d0
Thenemex <tnmx777@gmail.com>
2025-11-10 22:54:06 +0100

    Set version to 1.0.0.1

--- src/main/java/thaumcraftneipluginpatched/TCNEIPluginPatched.java
+++ src/main/java/thaumcraftneipluginpatched/TCNEIPluginPatched.java
@@ -1,29 +1,24 @@
 package thaumcraftneipluginpatched;
 
 import cpw.mods.fml.common.Mod;
-import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
-import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 import nemexlib.api.util.Logger;
+import thaumcraftneipluginpatched.model.config.ConfigIntegrations;
 
 import static thaumcraftneipluginpatched.TCNEIPluginPatched.dependencies;
 import static thaumcraftneipluginpatched.TCNEIPluginPatched.modID;
 
 @SuppressWarnings({"unused", "EmptyMethod"})
-@Mod(modid = modID, useMetadata = true, version = "1.0", dependencies = dependencies)
+@Mod(modid = modID, useMetadata = true, version = "1.0.0.1", dependencies = dependencies)
 public class TCNEIPluginPatched {
 
     public static final String modID = "thaumcraftneipluginpatched", modName = "TCNEIPluginPatched";
     public static final Logger logger = new Logger(modName);
 
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent ignoredEvent) {}
+    public void postInit(FMLPostInitializationEvent ignoredEvent) {
+        ConfigIntegrations.init();
+    }
 
-    @Mod.EventHandler
-    public void init(FMLInitializationEvent ignoredEvent) {}
-
-    @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent ignoredEvent) {}
-
-    public static final String dependencies = "required-after:thaumcraftneiplugin;required-after:NemexLib@[1.3.1.0.1,)";
+    public static final String dependencies = "required-after:CodeChickenCore@[1.0.7.48,);required-after:NotEnoughItems@[1.0.5.120,);required-after:thaumcraftneiplugin;required-after:NemexLib@[1.3.1.1,);after:TC4Patched@[1.4,)";
 }
